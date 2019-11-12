6f4abffccf40158abbbe256346c3753eaf1e88d5
Atulya <atulya2109@gmail.com>
2019-11-13 01:47:15 +0530

    Added Teacher Functionality

--- frontend/static/css/main.js
+++ frontend/static/css/main.js
@@ -3,15 +3,18 @@ function createTable(data)
 {   
     students = data.students;
     table = document.getElementById('tableBody')
-    var button = '<td><label>Present</label><input type="radio" name="set_status" value="Present" checked><label>Absent </label><input type="radio" name="set_status" value="Absent"></td>'
- 
+    // var button = '<td><label>Present</label><input type="radio" name="set_status" value="Present" checked><label>Absent </label><input type="radio" name="set_status" value="Absent"></td>'
+    var button = '<td><input type="checkbox"></td>' 
+
     table.innerHTML = "";
     for(var i = 0;i<students.length;i++)
     {   
         var trow = document.createElement('tr')
         var tid = document.createElement('td')
         tid.innerText = students[i].id
+        tid.setAttribute("name","id")
         var tname = document.createElement('td')
+        tname.setAttribute("name","st_name")
         tname.innerText = students[i].name
         trow.append(tid)
         trow.append(tname)
@@ -39,6 +42,54 @@ function getClassData()
     });
 }
 
+function getAttendanceData()
+{
+    trows = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+    var students = [];
+    var attend = {};
+    for(var i = 0;i<trows.length;i++)
+    {
+        var tds = trows[i].getElementsByTagName("td");
+        var user = {};
+        for(var j = 0;j<tds.length;j++)
+        {
+            
+            if (tds[j].getAttribute("name") == "id")
+            {
+                user['id'] = tds[i].innerText;
+            }
+        }
+
+        var checkbox = trows[i].getElementsByTagName("input")[0];
+        if (checkbox.checked)
+        {
+            user['present'] = true;
+        }
+        else
+            user['present'] = false;
+
+        
+        students.push(user);
+    }
+    attend['students'] = students;
+    return attend;
+}
+
+function postAttendanceData()
+{
+    data = getAttendanceData()
+
+    $.ajax({
+        url: '/markattendance',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: createTable,
+        data: JSON.stringify(classroom)
+    });
+}
+
 $(document).ready(function() {
     $('#submitButton')[0].addEventListener("click",getClassData);
+    $('#save')[0].addEventListener("click",postAttendanceData);
 });
